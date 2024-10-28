9e8753e793d0ab606295a7e3832d2867bddd7422
Jackson <jacksyntax20@gmail.com>
2024-10-28 19:58:04 -0300

    create sessionId with cookie

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,13 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
-
-interface TransactionProps {
-  id: string
-  title: string
-  amount: number
-}
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/', async () => {
-  const transaction = await knex<TransactionProps>('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app.listen({ port: env.PORT }).then(() => {
